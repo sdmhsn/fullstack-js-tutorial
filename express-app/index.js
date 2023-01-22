cece3493c2e2fec71b78ec42a4d1770329e45ab9
Saddam Husein <saddambinhusein2@gmail.com>
2023-01-22 18:10:00 +0800

    basic routing

--- express-app/index.js
+++ express-app/index.js
@@ -3,7 +3,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// GET
+app.get('/', (req, res) => {
+  res.send('request by GET method'); // .send() expressjs method. only can use once
+  // res.write('Hello '); // .write() nodejs method. can use multiple times
+  // res.write('World!');
+  res.end();
+});
+
+// POST
+app.post('/contoh', (req, res) => {
+  res.send('request by POST method');
+});
+
+// PUT
+app.put('/contoh', (req, res) => {
+  res.send('request by PUT method');
+});
+
+// DELETE
+app.delete('/contoh', (req, res) => {
+  res.send('request by DELETE method');
+});
+
+// ALL (this method depending to what request that we use)
+app.all('/universal', (req, res) => {
+  res.send(`request by ${req.method} method`);
+});
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
