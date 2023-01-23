7f650e27591965834e6e52dda1d5dbd4d820b648
Saddam Husein <saddambinhusein2@gmail.com>
2023-01-23 18:17:38 +0800

    created simple middleware: handling log

--- express-app/index.js
+++ express-app/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const routers = require('./routers');
-app.use(routers);
+const log = (req, res, next) => {
+  console.log(`${Date.now()} ${req.hostname} ${req.originalUrl}`);
+  next();
+};
+
+app.use(log);
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
