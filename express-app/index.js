0c2aa845fc8a6e2a3efef883586398ec0abeaba1
Saddam Husein <saddambinhusein2@gmail.com>
2023-01-24 12:41:54 +0800

    created simple middleware: handling error 500

--- express-app/index.js
+++ express-app/index.js
@@ -23,6 +23,17 @@ const notFound = (req, res, next) => {
 };
 app.use(notFound);
 
+// handling error
+const errorHandling = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.json({
+    status: 'error',
+    message: 'server error',
+  });
+};
+app.use(errorHandling);
+
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
