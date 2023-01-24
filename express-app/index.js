1980d197185b17bdfdf2a7e49c3f1e9ed267db1c
Saddam Husein <saddambinhusein2@gmail.com>
2023-01-24 11:32:08 +0800

    created simple middleware: handling 404 routing

--- express-app/index.js
+++ express-app/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// handling log middleware
 const log = (req, res, next) => {
   console.log(`${Date.now()} ${req.hostname} ${req.originalUrl}`);
   next();
 };
-
 app.use(log);
+
+// routing declaration
+const routers = require('./routers');
+app.use(routers);
+
+// handling 404 middleware
+const notFound = (req, res, next) => {
+  res.status(404);
+  res.json({
+    status: 'error',
+    message: 'resource not found',
+  });
+};
+app.use(notFound);
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
