f01c0b103cba2da348031abb95f625b69999cb02
Saddam Husein <saddambinhusein2@gmail.com>
2023-01-25 11:40:25 +0800

    handling file preview and download: preview the file

--- express-app/routers.js
+++ express-app/routers.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routers = express.Router();
+const path = require('path');
 
 routers.get('/', (req, res) => {
   res.send('Hello World!');
@@ -13,6 +14,16 @@ routers.post('/login', (req, res) => {
   // console.log(req.body);
   const { username, password } = req.body;
   res.send(`Anda login dengan username ${username} dan password ${password}`);
+});
+
+routers.get('/download', (req, res) => {
+  const filename = 'logo.png';
+  // Alternative 1:
+  // res.sendFile(__dirname + '/' + filename);
+  // Alternative 2:
+  // res.sendFile(path.resolve(__dirname, filename));
+  // Alternative 3:
+  res.sendFile(path.join(__dirname, filename));
 });
 
 module.exports = routers;
