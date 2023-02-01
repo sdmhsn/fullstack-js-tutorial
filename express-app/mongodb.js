e21e1ebf048da141014507947a5e390a6235fdb3
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-01 12:20:29 +0800

    mongodb connection test: async - await

--- express-app/mongodb.js
+++ express-app/mongodb.js
@@ -5,6 +5,13 @@ const MongoClient = require('mongodb').MongoClient;
 const connectionString =
   'mongodb://user_latihan:123456@localhost:27017?authSource=admin'; // mongodb://<user>:<password>@localhost:27017?authSource=admin
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then((res) => console.log('Server database connect!'))
-  .catch((err) => console.log('Connection failed!'));
+(async () => {
+  try {
+    let res = await MongoClient.connect(connectionString, {
+      useUnifiedTopology: true,
+    });
+    console.log('Server database connect!');
+  } catch (error) {
+    console.log('Connection failed!');
+  }
+})(); // (); = calling anonymous function
