5798ab5ac21a1369934e952e7debf2cded96a92d
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-01 12:05:03 +0800

    mongodb connection test: callback

---
+++ express-app/mongodb.js
@@ -0,0 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// const connectionString = 'mongodb://localhost:27017?authSource=admin'; // without authentication
+
+const connectionString =
+  'mongodb://user_latihan:123456@localhost:27017?authSource=admin'; // mongodb://<user>:<password>@localhost:27017?authSource=admin
+
+MongoClient.connect(
+  connectionString,
+  { useUnifiedTopology: true },
+  (error, client) => {
+    if (error) return console.error('Connection failed!');
+    console.log('Server database connect!');
+  }
+);
