a129f92f6e73466ffdc1523beedbff63103e271c
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-07 10:26:01 +0800

    getting started mongoose

--- odm-intro/mongoose.js
+++ odm-intro/mongoose.js
@@ -3,22 +3,33 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
 // version 6:
-main()
-  .then((res) => console.log('Server database connect!'))
-  .catch((err) => console.log(err));
+main();
 
 async function main() {
-  await mongoose.connect(
-    'mongodb://user_latihan:123456@localhost:27017/latihan?authSource=admin'
-  );
-}
+  try {
+    await mongoose.connect(
+      'mongodb://user_latihan:123456@localhost:27017/latihan?authSource=admin'
+    );
 
-// version 5:
-// mongoose.connect(
-//   'mongodb://user_latihan:123456@localhost:27017/latihan?authSource=admin'
-// );
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', () => {
-//   console.log('Server database connect!');
-// });
+    // define a scheme
+    const quoteSchema = new mongoose.Schema({
+      word: String,
+    });
+
+    // create a model
+    const Quote = mongoose.model('Quote', quoteSchema); // 'Quote': collection in mongodb. Automatically plural and lowercased in mongodb. e.g. Quote to quotes
+
+    // create a new document
+    const quote = new Quote({
+      word: 'Man behind the gun',
+    });
+
+    // save a created document
+    quote.save((error, quote) => {
+      if (error) return console.error(error); // e.g. error: { _id: false } in define a schema. source: option: _id in https://mongoosejs.com/docs/guide.html#_id
+      console.log(quote);
+    });
+  } catch (error) {
+    console.log(error); // e.g. error: wrong authentication
+  }
+}
