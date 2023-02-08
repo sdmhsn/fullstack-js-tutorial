8af47c46e62d10662778d9bd9eef8141ced26bd9
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-08 15:40:40 +0800

    without validation

--- odm-intro/mongoose.js
+++ odm-intro/mongoose.js
@@ -22,36 +22,10 @@ async function main() {
     // create a model
     const Product = mongoose.model('Product', productSchema); // 'Product': collection in mongodb. Automatically plural and lowercased in mongodb. e.g. Product to products
 
-    // Read all data:
-    // const products = await Product.find();
-    // console.log(products);
-
-    // Read data by id:
-    // const product = await Product.findOne({ _id: '63df45936b10a351a0982f2f' });
-    // console.log(product);
-
-    // Create data:
-    // const product = await Product.create({
-    //   name: 'VGA Nvidia',
-    //   price: 1500000,
-    //   stock: 3,
-    //   status: true,
-    // });
-    // console.log(product);
-
-    // Update data:
-    // const product = await Product.updateOne(
-    //   { _id: '63df45936b10a351a0982f2f' },
-    //   { name: 'Computer Desk', price: 2000000 }
-    // );
-    // console.log(product);
-
-    // Delete data:
-    const product = await Product.deleteOne({
-      _id: '63e33d4b58a5fe9fdaa28580',
-    });
-    console.log(product);
+    // create a document using query
+    const newProduct = await Product.create({}); // without any field (blank). the document still created with _id and default status fields.
+    console.log(newProduct);
   } catch (error) {
-    console.log(error); // e.g. error: wrong authentication
+    console.log(error.message); // e.g. error: wrong authentication
   }
 }
