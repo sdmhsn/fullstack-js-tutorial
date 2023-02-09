71b6bcec96515942ae18d50da2eadc2789e9ea5f
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-09 11:37:41 +0800

    validation min and max

--- odm-intro/mongoose.js
+++ odm-intro/mongoose.js
@@ -12,19 +12,30 @@ async function main() {
     );
 
     // define a scheme
-    const productSchema = new mongoose.Schema({
-      name: { type: String, required: [true, 'Field name wajib!'] },
-      price: { type: Number, required: [true, 'Field price wajib!'] },
-      stock: Number,
-      status: { type: Boolean, default: true }, // default: true: set default value as true for status
-    });
+    const productSchema = new mongoose.Schema(
+      // Built-in Validators source: https://mongoosejs.com/docs/validation.html#built-in-validators
+      {
+        name: { type: String, required: true, minLength: 3, maxLength: 5 },
+        price: {
+          type: Number,
+          required: true,
+          min: [3, 'angka kurang dari 3'],
+          max: 5,
+        },
+        stock: Number,
+        status: { type: Boolean, default: true }, // default: true: set default value as true for status
+      }
+    );
 
     // create a model
     const Product = mongoose.model('Product', productSchema); // 'Product': collection in mongodb. Automatically plural and lowercased in mongodb. e.g. Product to products
 
     try {
       // create a document using query
-      const newProduct = await Product.create({});
+      const newProduct = await Product.create({
+        name: 'PC',
+        price: 0.1,
+      });
       console.log(newProduct);
     } catch (error) {
       // console.log(error.errors['name'].message);
