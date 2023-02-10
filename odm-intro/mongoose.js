d74a7e1bd141e4df0048c378b525289682584365
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-10 10:35:35 +0800

    queries: combine some methods (short)

--- odm-intro/mongoose.js
+++ odm-intro/mongoose.js
@@ -23,20 +23,13 @@ async function main() {
     const Product = mongoose.model('Product', productSchema); // 'Product': collection in mongodb. Automatically plural and lowercased in mongodb. e.g. Product to products
 
     // queries
-    const query = Product.find(); // find all documents
-    // queries: where() method
-    query.where({ stock: { $lte: 2 } }); // get specific documents by stock field. lte: lower than equal
+    const productResult = await Product.find()
+      .where({ stock: { $lte: 2 } })
+      .select('name stock')
+      .limit(3)
+      .sort({ stock: -1 })
+      .exec();
 
-    // queries: select() method
-    query.select('name stock'); // specifies which document fields (name and stock fields)
-
-    // queries: limit() method
-    query.limit(3); // Specifies the first maximum number of documents
-
-    // queries: sort() method
-    query.sort({ stock: -1 }); // sort by stock field by descending (-1: greater to lower)
-
-    const productResult = await query.exec();
     console.log(productResult);
   } catch (error) {
     console.log(error); // e.g. error: wrong authentication
