d2589bccdbdf2a50f41e1b4efc909622a924f793
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-12 14:47:56 +0800

    CRUD: Read all products

--- project-express-app-moongose/routes.js
+++ project-express-app-moongose/routes.js
@@ -3,4 +3,22 @@ const routers = express.Router();
 require('./connection');
 const Product = require('./product');
 
+// get all products (Read):
+routers.get('/products', async (req, res) => {
+  const products = await Product.find();
+
+  if (products.length > 0) {
+    res.send({
+      status: 'success',
+      message: 'List of products found',
+      data: products,
+    });
+  } else {
+    res.send({
+      status: 'warning',
+      message: 'Products not found! :)',
+    });
+  }
+});
+
 module.exports = routers;
