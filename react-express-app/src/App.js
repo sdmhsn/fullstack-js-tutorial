13041be3e1033b0b5d06e170a462b99b9a843ae3
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-19 18:15:02 +0800

    created App.js and App.css

---
+++ react-express-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Menu */}
+
+      {/* Main */}
+    </div>
+  );
+}
+
+export default App;
