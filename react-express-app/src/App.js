b609ca7d7acd9cf082f9de11cbbaa0d91cde64d2
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-19 18:49:34 +0800

    wrap App component using <React.Suspense> to implement React.lazy() in routes.js

--- react-express-app/src/App.js
+++ react-express-app/src/App.js
@@ -6,39 +6,41 @@ import routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      {/* Menu */}
-      <ul className="menu">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-            end
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/products"
-            className={({ isActive }) => (isActive ? 'active' : '')}
-          >
-            Product
-          </NavLink>
-        </li>
-      </ul>
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <div className="App">
+        {/* Menu */}
+        <ul className="menu">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? 'active' : '')}
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              Product
+            </NavLink>
+          </li>
+        </ul>
 
-      {/* Main */}
-      <div className="main">
-        <Routes>
-          {routes.map((route, i) => {
-            // console.log(route);
-            const { path, Component } = route;
-            return <Route key={i} path={path} element={<Component />} />;
-          })}
-        </Routes>
+        {/* Main */}
+        <div className="main">
+          <Routes>
+            {routes.map((route, i) => {
+              // console.log(route);
+              const { path, Component } = route;
+              return <Route key={i} path={path} element={<Component />} />;
+            })}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </React.Suspense>
   );
 }
 
