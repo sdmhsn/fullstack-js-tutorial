9696e0e56321a4b41726669679b4caf3882f0437
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-19 23:56:26 +0800

    CRUD: Read All Products - used UseEffect() hook and Axios to get data products from backend

--- react-express-app/src/Product/List.js
+++ react-express-app/src/Product/List.js
@@ -2,6 +2,28 @@ import React from 'react';
 import axios from 'axios';
 
 const List = () => {
+  React.useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/products');
+        // console.log(response.data);
+
+        const { status, message, data } = response.data;
+
+        // handle data list product
+        if (status === 'success') {
+          console.log(data);
+        } else {
+          throw Error(message);
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+
+    getProducts();
+  }, []); // no dependency required
+
   return (
     <>
       <h2>Products List Page</h2>
