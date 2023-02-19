03498877919ca159026b8ba1f14a8c03b39f5c08
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-19 18:18:51 +0800

    created app menus (Home and Product) in App.js

--- react-express-app/src/App.js
+++ react-express-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './App.css';
 
@@ -6,6 +7,25 @@ function App() {
   return (
     <div className="App">
       {/* Menu */}
+      <ul className="menu">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/products"
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            Product
+          </NavLink>
+        </li>
+      </ul>
 
       {/* Main */}
     </div>
