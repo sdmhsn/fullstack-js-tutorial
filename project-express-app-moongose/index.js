2670769ea8b7141cef2a28ee52ac750ce893df20
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-19 14:43:03 +0800

    added backend and code revision - installed cors package - added express cors middleware to web server in index.js - deleted patch update router in routes.js

---
+++ project-express-app-moongose/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const routers = require('./routes');
+const cors = require('cors');
+
+// handling cors
+// alternative 1:
+// app.use(cors());
+
+// alternative 2:
+const corsOptions = {
+  origin: 'http://localhost:4000', // react
+};
+app.use(cors(corsOptions));
+
+// handling request body (should before app.use(routers) / routing declaration statements)
+app.use(express.urlencoded({ extended: true })); // parse x-www-form-url-encode
+app.use(express.json()); // parse JSON
+
+// routing declaration statements
+app.use(routers);
+
+// handling 404 middleware
+const notFound = (req, res, next) => {
+  res.status(404);
+  res.json({
+    status: 'error',
+    message: 'resource not found',
+  });
+};
+app.use(notFound);
+
+// handling error
+const errorHandling = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.json({
+    status: 'error',
+    message: 'server error',
+  });
+};
+app.use(errorHandling);
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
---
+++ project-express-app-moongose/routes.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const routers = express.Router();
+require('./connection');
+const Product = require('./product');
+const multer = require('multer');
+
+// get all products (Read):
+routers.get('/products', async (req, res) => {
+  const products = await Product.find();
+
+  if (products.length > 0) {
+    res.send({
+      status: 'success',
+      message: 'List of products found',
+      data: products,
+    });
+  } else {
+    res.send({
+      status: 'warning',
+      message: 'Products not found! :)',
+    });
+  }
+});
+
+// get single product (Read by id):
+routers.get('/product/:id', async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+
+  if (product) {
+    res.send({
+      status: 'success',
+      message: 'Single product found',
+      data: product,
+    });
+  } else {
+    res.send({
+      status: 'warning',
+      message: 'Product not found! :)',
+    });
+  }
+});
+
+// post new product (Create):
+routers.post('/product', multer().none(), async (req, res) => {
+  // multer(): to handling request body from form-data
+  try {
+    const { name, price, stock, status } = req.body;
+    const product = await Product.create({
+      name,
+      price,
+      stock,
+      status,
+    });
+
+    if (product) {
+      res.send({
+        status: 'success',
+        message: 'Added new product',
+        data: product,
+      });
+    } else {
+      res.send({
+        status: 'warning',
+        message: 'Product not added',
+      });
+    }
+  } catch (error) {
+    res.send({
+      status: 'error',
+      message: error.message,
+    });
+  }
+});
+
+// put product (Update):
+routers.put('/product/:id', multer().none(), async (req, res) => {
+  try {
+    const { name, price, stock, status } = req.body;
+    const id = req.params.id;
+    const product = await Product.updateOne(
+      { _id: id },
+      { name, price, stock, status },
+      { runValidators: true }
+    );
+
+    // console.log(product);
+
+    if (product.matchedCount === 1) {
+      res.send({
+        status: 'success',
+        message: 'Updated product',
+        data: { _id: id, ...req.body },
+      });
+    } else {
+      res.send({
+        status: 'warning',
+        message: 'Product updated failed :D :D!',
+      });
+    }
+  } catch (error) {
+    res.send({
+      status: 'error',
+      message: error.message,
+    });
+  }
+});
+
+// delete product (Delete):
+routers.delete('/product/:id', async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.deleteOne({
+    _id: id,
+  });
+
+  // console.log(product);
+
+  if (product.deletedCount === 1) {
+    res.send({
+      status: 'success',
+      message: 'Product deleted',
+    });
+  } else {
+    res.send({
+      status: 'warning',
+      message: 'Product deleted failed! :)',
+    });
+  }
+});
+
+module.exports = routers;
