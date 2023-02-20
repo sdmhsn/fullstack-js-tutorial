c2d7b722f692caa95ffb165beebdf5a5c4ede2f3
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-20 12:52:49 +0800

    CRUD: Read Single Product - created back button using useNavigate()

--- react-express-app/src/Product/Single.js
+++ react-express-app/src/Product/Single.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const Single = () => {
   const [product, setProduct] = React.useState({});
   const { productId } = useParams(); // productId: related to /products/single/:productId path in routes
+  const navigate = useNavigate(); // use useNavigate() in v6 instead of useHistory() in v5.
 
   React.useEffect(() => {
     const getProduct = async () => {
@@ -45,6 +46,7 @@ const Single = () => {
           <div>Status: {product.status ? 'on' : 'off'}</div>
         </>
       )}
+      <button onClick={() => navigate('/products')}>&laquo; Back</button>
     </>
   );
 };
