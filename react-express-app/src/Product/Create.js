99cddd1552776043aabc2e0811737464cb0f8541
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-20 17:09:49 +0800

    CRUD: Create Product - created product state with default properties

--- react-express-app/src/Product/Create.js
+++ react-express-app/src/Product/Create.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Create = () => {
+  const [product, setProduct] = React.useState({
+    name: '',
+    price: 0,
+    stock: 0,
+    status: false, // default chacked off
+  });
+
   return (
     <>
       <h2>Create Product Form Page</h2>
