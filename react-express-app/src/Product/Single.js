958eba5118e868c2d39a7f0a9bdad8afb985eb27
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-20 12:21:58 +0800

    CRUD: Read Single Product - added useParams() to return productId in URL

--- react-express-app/src/Product/Single.js
+++ react-express-app/src/Product/Single.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Single = () => {
+  const { productId } = useParams(); // productId: related to /products/single/:productId path in routes
+
   return (
     <>
       <h2>Single Product Page</h2>
