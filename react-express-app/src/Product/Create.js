058f616d4ad935cab68428706d622fdb3f0eae83
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-20 17:15:54 +0800

    CRUD: Create Product - created product form on page

--- react-express-app/src/Product/Create.js
+++ react-express-app/src/Product/Create.js
@@ -11,6 +11,27 @@ const Create = () => {
   return (
     <>
       <h2>Create Product Form Page</h2>
+
+      <form>
+        <label htmlFor="product_name">Name:</label>
+        <input type="text" id="product_name" name="name" />
+
+        <label htmlFor="product_price">Price:</label>
+        <input type="number" id="product_price" name="price" />
+
+        <label htmlFor="product_stock">Stock:</label>
+        <input type="number" id="product_stock" name="stock" />
+
+        <label>Status:</label>
+        <div>
+          <input type="radio" name="status" id="on" />
+          <label htmlFor="on">On</label>
+          <input type="radio" name="status" id="off" />
+          <label htmlFor="off">Off</label>
+        </div>
+
+        <button>Submit</button>
+      </form>
     </>
   );
 };
