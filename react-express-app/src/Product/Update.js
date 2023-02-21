a5f49b05c147a11b4856d86a9a0e92722693aab7
Saddam Husein <saddambinhusein2@gmail.com>
2023-02-21 11:29:09 +0800

    CRUD: Update Product - added React onChange event attributes and event handler method (handleInputChange())

--- react-express-app/src/Product/Update.js
+++ react-express-app/src/Product/Update.js
@@ -1,25 +1,52 @@
 import React from 'react';
 
 const Update = () => {
+  const handleInputChange = (event) => {};
+
   return (
     <>
       <h2>Update Product Form Page</h2>
 
       <form>
         <label htmlFor="product_name">Name:</label>
-        <input type="text" id="product_name" name="name" />
+        <input
+          type="text"
+          id="product_name"
+          name="name"
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="product_price">Price:</label>
-        <input type="number" id="product_price" name="price" />
+        <input
+          type="number"
+          id="product_price"
+          name="price"
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="product_stock">Stock:</label>
-        <input type="number" id="product_stock" name="stock" />
+        <input
+          type="number"
+          id="product_stock"
+          name="stock"
+          onChange={handleInputChange}
+        />
 
         <label>Status:</label>
         <div>
-          <input type="radio" name="status" id="on" />
+          <input
+            type="radio"
+            name="status"
+            id="on"
+            onChange={handleInputChange}
+          />
           <label htmlFor="on">On</label>
-          <input type="radio" name="status" id="off" />
+          <input
+            type="radio"
+            name="status"
+            id="off"
+            onChange={handleInputChange}
+          />
           <label htmlFor="off">Off</label>
         </div>
 
